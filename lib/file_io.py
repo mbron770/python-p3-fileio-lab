943fdbe49b0fcdf9261a87dfdaa5f63bcfcd3a53
mbron770 <mbronfin.tsd@gmail.com>
2023-07-10 00:19:13 -0400

    a

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
-
+    with open(f'{file_name}.txt', 'w') as f:
+        f.write(file_content)
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', 'a') as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt') as f: 
+        return  f.read()
+
+write_file(file_name="logfile.txt", file_content="Log 1: 5 bananas added")
+append_file(file_name="logfile.txt", append_content="Log 2: 3 bananas subtracted")
+read_file(file_name="logfile.txt")
